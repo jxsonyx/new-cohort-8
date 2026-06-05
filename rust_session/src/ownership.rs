d8e5a6c4794b857ee8a02c7d75db08cce6122cf7
sprtd <sagesdn@gmail.com>
2026-06-05 14:38:58 +0100

    chore: implement move and clone

--- rust_session/src/ownership.rs
+++ rust_session/src/ownership.rs
@@ -10,3 +10,13 @@ pub fn call_name() {
     
     println!("name is: {}", name);
 }
+
+
+fn test_move() {
+    let a = String::from("Anagkazo");
+    println!("{}", a.clone()); // ✅ compiles: another address has been created in memory
+    let b = a;          // ownership moves from `a` to `b`
+
+    // println!("{}", a); // ❌ compile error: `a` was moved
+    println!("{}", b);    // ✅ `b` is the owner now
+}
