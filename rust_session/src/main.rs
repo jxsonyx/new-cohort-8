4022ea378abcdd316276935405cc5d6ba543fe54
sprtd <sagesdn@gmail.com>
2026-06-05 14:07:32 +0100

    chore: add array

--- rust_session/src/main.rs
+++ rust_session/src/main.rs
@@ -1,8 +1,13 @@
 mod sub;
 mod sum;
 mod ownership;
+mod array;
+
 
 fn main() {
-    use sub::sub;
-    sub(10, 5);
+    // use sub::sub;
+    // sub(10, 5);
+    // ownership::test_ownership();
+    // ownership::call_name();
+    array::test_array();
 }
--- rust_session/src/ownership.rs
+++ rust_session/src/ownership.rs
@@ -1,5 +1,12 @@
 pub fn test_ownership() {
     let name = String::from("Alice"); // `name` is the owner of this String
-    println!("{}", name);
+    // name.push('r');
+    println!("name is: {}", name);
 }
 
+pub fn call_name() {
+    let name: &str = "Yusrah";
+    // name.
+    
+    println!("name is: {}", name);
+}
